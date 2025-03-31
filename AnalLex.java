06f5f0e95b3ead483e1fb7e01608ed69f5a9d263
Mj-gagne <gagm1108@usherbrooke.ca>
2025-03-31 14:31:53 -0400

    save au cas ou

--- AnalLex.java
+++ AnalLex.java
@@ -6,104 +6,158 @@ package app6;
  */
 public class AnalLex {
 
-  // Attributs
-  private int ptrLecture;
+// Attributs
+//  ...
   private String input;
-  private int state;
+  private int ptr;
+  private int etat;
 
 
-  /**
-   * Constructeur pour l'initialisation d'attribut(s)
+  /** Constructeur pour l'initialisation d'attribut(s)
    */
-  public AnalLex(String input) {  // arguments possibles
-    this.ptrLecture = 0;
-    this.input = input;
-    this.state = 0;
+  public AnalLex(String string ) {  // arguments possibles
+    //
+    this.ptr = 0;
+    this.input = string;
+    this.etat = 0;
   }
 
 
-  /**
-   * resteTerminal() retourne :
-   * false  si tous les terminaux de l'expression arithmetique ont ete retournes
-   * true s'il reste encore au moins un terminal qui n'a pas ete retourne
+  /** resteTerminal() retourne :
+   false  si tous les terminaux de l'expression arithmetique ont ete retournes
+   true s'il reste encore au moins un terminal qui n'a pas ete retourne
    */
-  public boolean resteTerminal() {
-    return ptrLecture < input.length();
+  public boolean resteTerminal( ) {
+    //
+    if(ptr < input.length() - 1 && Character.isWhitespace(input.charAt(ptr))) return false;
+    return ptr < input.length();
   }
 
+  public char lectureChar(){
+    int temp = ptr;
+    ptr++;
+    return input.charAt(temp);
+  }
 
-  /**
-   * prochainTerminal() retourne le prochain terminal
-   * Cette methode est une implementation d'un AEF
+  public boolean isOperator(char c){
+    return c == '+' || c == '-' || c == '/' || c == '*';
+  }
+
+  public boolean isNumber(char c){
+    return c >= '0' && c <= '9';
+  }
+
+  public boolean isMajuscule(char c){
+    return c >= 'A' && c <= 'Z';
+  }
+
+  public boolean isMinuscule(char c){
+    return c >= 'a' && c <= 'z';
+  }
+
+  public boolean isLetter(char c){
+    return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
+  }
+
+  /** prochainTerminal() retourne le prochain terminal
+   Cette methode est une implementation d'un AEF
    */
-  public Terminal prochainTerminal() throws Exception {
-    String terminalReturn = "";
-    while (resteTerminal()) {
-      char currentChar = input.charAt(ptrLecture);
-      ptrLecture++;
-      switch (state) {
+  public Terminal prochainTerminal( ) {
+    //
+    char c;
+    Terminal terminal = new Terminal();
+    //boolean next = true;
+
+    while(next && resteTerminal()){
+      switch(etat){
         case 0:
-          if (currentChar == '+') {
-            terminalReturn += "+";
-            return new Terminal(terminalReturn);
+          c = lectureChar();
+          if(isMajuscule(c)){
+            terminal.input += c;
+            etat = 2;
           }
-          if (currentChar == '0' || currentChar == '1') {
-            state = 1;
-            terminalReturn += currentChar;
-            break;
+          else if (isNumber(c)) {
+            terminal.input += c;
+            etat = 1;
           }
-          ErreurLex("Erreur");
-          return null;
+          else if (isOperator(c)) {
+            terminal.input += c;
+            //next = false;
+          }
+          else{
+            ErreurLex("big mistake");
+          }
+          //case numbers
         case 1:
-          if (currentChar == '0' || currentChar == '1') {
-            terminalReturn += currentChar;
-            break;
-          } else {
-            ptrLecture--;
-            state = 0;
-            return new Terminal(terminalReturn);
+          c = lectureChar();
+          if(isNumber(c)){
+            terminal.input += c;
+            etat = 1;
+          } else if (isOperator(c) || Character.isWhitespace(c) ) {
+            ptr--;
+            return terminal; //checker
+            //next = false;
+          }
+          else {
+            ErreurLex("sortie etat 1");
+          }
+        case 2:
+          c = lectureChar();
+          if(isLetter(c)){
+            etat = 2;
+            terminal.input += c;
+          }
+          else if(isOperator(c) || Character.isWhitespace(c)){
+            ptr--;
+          } else if (c == '_') {
+            terminal.input += c;
+            etat = 3;
+          }
+          else{
+            ErreurLex("sortie etat 2");
+          }
+        case 3:
+          c = lectureChar();
+          if(isLetter(c)){
+            terminal.input += c;
+            etat = 2;
+          }
+          else{
+            ErreurLex("Sortie etat 3");
           }
       }
     }
-    return new Terminal(terminalReturn);
   }
 
 
-  /**
-   * ErreurLex() envoie un message d'erreur lexicale
+  /** ErreurLex() envoie un message d'erreur lexicale
    */
-  public void ErreurLex(String s) throws Exception {
-    System.err.println("ErreurLex: " + s);
-    throw new Exception(s);
+  public void ErreurLex(String s) {
+    system.out.println(s);
   }
 
 
   //Methode principale a lancer pour tester l'analyseur lexical
   public static void main(String[] args) {
-    try {
-      String toWrite = "";
-      System.out.println("Debut d'analyse lexicale");
-      if (args.length == 0) {
-        args = new String[2];
-        args[0] = "ExpArith.txt";
-        args[1] = "ResultatLexical.txt";
-      }
-      Reader r = new Reader(args[0]);
-
-      AnalLex lexical = new AnalLex(r.toString()); // Creation de l'analyseur lexical
-
-      // Execution de l'analyseur lexical
-      Terminal t = null;
-      while (lexical.resteTerminal()) {
-        t = lexical.prochainTerminal();
-        toWrite += t.chaine + "\n";  // toWrite contient le resultat
-      }
-      //    d'analyse lexicale
-      System.out.println(toWrite);    // Ecriture de toWrite sur la console
-      Writer w = new Writer(args[1], toWrite); // Ecriture de toWrite dans fichier args[1]
-      System.out.println("Fin d'analyse lexicale");
-    } catch (Exception e) {
-      System.out.println(e.getMessage());
+    String toWrite = "";
+    System.out.println("Debut d'analyse lexicale");
+    if (args.length == 0){
+      args = new String [2];
+      args[0] = "ExpArith.txt";
+      args[1] = "ResultatLexical.txt";
     }
+    Reader r = new Reader(args[0]);
+
+    AnalLex lexical = new AnalLex(r.toString()); // Creation de l'analyseur lexical
+
+    // Execution de l'analyseur lexical
+    Terminal t = null;
+    while(lexical.resteTerminal()){
+      t = lexical.prochainTerminal();
+      toWrite +=t.chaine + "\n" ;  // toWrite contient le resultat
+    }				   //    d'analyse lexicale
+    System.out.println(toWrite); 	// Ecriture de toWrite sur la console
+    Writer w = new Writer(args[1],toWrite); // Ecriture de toWrite dans fichier args[1]
+    System.out.println("Fin d'analyse lexicale");
   }
 }
